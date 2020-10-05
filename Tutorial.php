e9466a5e231a9dd573ca5006425158b0588d9f63
FalahRafif <frthegamer95@gmail.com>
2020-10-05 20:40:54 +0700

    About git

---
+++ Tutorial.php
@@ -0,0 +1,29 @@
+<?php
+
+class Tutorial {
+    
+    public function About_Git()
+    {
+        /*
+        Git adalah salah satu sistem pengontrol versi(Version Control System) 
+        Fungsi git mencatat setiap perubahan pada file yang di amati oleh git
+
+        Git dikenal juga dengan distributed revision control (VCS terdistribusi), 
+        dimana penyimpanan database Git tidak hanya di satu tempat saja.
+
+        BIasanya git di gunakan untuk membuat projek tim
+
+        Manfaat git :
+        - Bisa menyimpan seluruh versi source code;
+        - Bisa paham cara kolaborasi dalam proyek;
+        - Bisa ikut berkontribusi ke poryek open-source;
+        - Lebih aman digunakan untuk kolaborasi, karena kita bisa tahu apa yang diubah dan siapa yang mengubahnya.
+        - Bisa memahami cara deploy aplikasi modern;
+        - Bisa membuat blog dengan SSG.
+        */
+    }
+}
+
+
+
+?>
